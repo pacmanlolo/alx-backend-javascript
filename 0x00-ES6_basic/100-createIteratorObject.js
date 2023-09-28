773b526fba239a862f1ddbc7833d5c761eb7e2b9
pacmanlolo <michael.omotosho7@gmail.com>
2023-09-28 15:05:26 +0000

    Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,23 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+  for (const department in report.allEmployees) {
+    employees.push(...report.allEmployees[department]);
+  }
+
+  let currentIndex = 0;
+
+  return {
+    next() {
+      if (currentIndex < employees.length) {
+        return {
+          value: employees[currentIndex++],
+          done: false,
+        };
+      } else {
+        return {
+          done: true,
+        };
+      }
+    },
+  };
+}
