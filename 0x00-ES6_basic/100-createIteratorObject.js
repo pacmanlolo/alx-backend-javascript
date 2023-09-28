0cf287ed105b409f0b4b331751ea09b18a4bf2be
pacmanlolo <michael.omotosho7@gmail.com>
2023-09-28 15:18:21 +0000

    Updates

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,23 +1,26 @@
 export default function createIteratorObject(report) {
   const employees = [];
+  
   for (const department in report.allEmployees) {
     employees.push(...report.allEmployees[department]);
   }
 
   let currentIndex = 0;
 
-  return {
-    next() {
-      if (currentIndex < employees.length) {
-        return {
-          value: employees[currentIndex++],
-          done: false,
-        };
-      } else {
-        return {
-          done: true,
-        };
-      }
-    },
+  return function iterator() {
+    return {
+      next() {
+        if (currentIndex < employees.length) {
+          return {
+            value: employees[currentIndex++],
+            done: false,
+          };
+        } else {
+          return {
+            done: true,
+          };
+        }
+      },
+    };
   };
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
-   let myItems = '', i = 0;
-   for (const it of reportWithIterator) {
-        if (reportWithIterator.length - 1 == i) {
-            myItems += it
-        }
-        else {
-            myItems += it + ' | ';
-        }
-        i++;
-   }
-   return myItems;
+  const iterator = reportWithIterator();
+
+  const employeeNames = [];
+  let next = iterator.next();
+
+  while (!next.done) {
+    employeeNames.push(next.value);
+    next = iterator.next();
+  }
+
+  return employeeNames.join(' | ');
 }
