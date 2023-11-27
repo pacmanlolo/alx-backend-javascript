b5a31f4d8773f3ef93784cf145436e047eb18aff
pacmanlolo <michael.omotosho7@gmail.com>
2023-11-27 08:39:54 +0000

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+// Function to get payment tokens from API
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
+
+module.exports = getPaymentTokenFromAPI;
