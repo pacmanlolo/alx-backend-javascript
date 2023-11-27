adaf2e6a17a4691b669d5f921b9f511bdb958101
pacmanlolo <michael.omotosho7@gmail.com>
2023-11-27 08:36:08 +0000

    Spies

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,9 @@
+// Send payment function
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
+
+module.exports = sendPaymentRequestToApi;
